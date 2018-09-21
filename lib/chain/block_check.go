e0c984da6ff6b128a19a7e2d519ad58f58a09dd7
Sumanth Kumar <sumanth.meesala@blockfreight.com>
2018-09-21 17:53:49 +1000

    BCH precheck block function added

--- lib/chain/block_check.go
+++ lib/chain/block_check.go
@@ -12,6 +12,102 @@ import (
 )
 
 // Make sure to call this function with ch.BlockIndexAccess locked
+func (ch *Chain) BCHPreCheckBlock(bl *btc.Block) (er error, dos bool, maybelater bool) {
+	// Size limits
+	if len(bl.Raw) < 81 {
+		er = errors.New("CheckBlock() : size limits failed - RPC_Result:bad-blk-length")
+		dos = true
+		return
+	}
+
+	ver := bl.Version()
+	if ver == 0 {
+		er = errors.New("CheckBlock() : Block version 0 not allowed - RPC_Result:bad-version")
+		dos = true
+		return
+	}
+
+	// Check proof-of-work
+	if !btc.CheckProofOfWork(bl.Hash, bl.Bits()) {
+		er = errors.New("CheckBlock() : proof of work failed - RPC_Result:high-hash")
+		dos = true
+		return
+	}
+
+	// Check timestamp (must not be higher than now +2 hours)
+	if int64(bl.BlockTime()) > time.Now().Unix()+2*60*60 {
+		er = errors.New("CheckBlock() : block timestamp too far in the future - RPC_Result:time-too-new")
+		dos = true
+		return
+	}
+
+	if prv, pres := ch.BlockIndex[bl.Hash.BIdx()]; pres {
+		if prv.Parent == nil {
+			// This is genesis block
+			er = errors.New("Genesis")
+			return
+		} else {
+			er = errors.New("CheckBlock: " + bl.Hash.String() + " already in - RPC_Result:duplicate")
+			return
+		}
+	}
+
+	prevblk, ok := ch.BlockIndex[btc.NewUint256(bl.ParentHash()).BIdx()]
+	if !ok {
+		er = errors.New("CheckBlock: " + bl.Hash.String() + " parent not found - RPC_Result:bad-prevblk")
+		maybelater = true
+		return
+	}
+
+	bl.Height = prevblk.Height + 1
+
+	// Reject the block if it reaches into the chain deeper than our unwind buffer
+	lst_now := ch.LastBlock()
+	if prevblk != lst_now && int(lst_now.Height)-int(bl.Height) >= MovingCheckopintDepth {
+		er = errors.New(fmt.Sprint("CheckBlock: btc.Block ", bl.Hash.String(),
+			" hooks too deep into the chain: ", bl.Height, "/", lst_now.Height, " ",
+			btc.NewUint256(bl.ParentHash()).String(), " - RPC_Result:bad-prevblk"))
+		return
+	}
+
+	// Check proof of work
+	gnwr := ch.GetNextWorkRequired(prevblk, bl.BlockTime())
+	if bl.Bits() != gnwr {
+		er = errors.New("CheckBlock: incorrect proof of work - RPC_Result:bad-diffbits")
+		dos = true
+		return
+	}
+
+	// Check timestamp against prev
+	bl.MedianPastTime = prevblk.GetMedianTimePast()
+	if bl.BlockTime() <= bl.MedianPastTime {
+		er = errors.New("CheckBlock: block's timestamp is too early - RPC_Result:time-too-old")
+		dos = true
+		return
+	}
+
+	if ver < 2 && bl.Height >= ch.Consensus.BIP34Height ||
+		ver < 3 && bl.Height >= ch.Consensus.BIP66Height ||
+		ver < 4 && bl.Height >= ch.Consensus.BIP65Height {
+		// bad block version
+		erstr := fmt.Sprintf("0x%08x", ver)
+		er = errors.New("CheckBlock() : Rejected Version=" + erstr + " block - RPC_Result:bad-version(" + erstr + ")")
+		dos = true
+		return
+	}
+
+	if ch.Consensus.BIP91Height != 0 && ch.Consensus.Enforce_SEGWIT != 0 {
+		if bl.Height >= ch.Consensus.BIP91Height && bl.Height < ch.Consensus.Enforce_SEGWIT-2016 {
+			if (ver&0xE0000000) != 0x20000000 || (ver&2) == 0 {
+				er = errors.New("CheckBlock() : relayed block must signal for segwit - RPC_Result:bad-no-segwit")
+			}
+		}
+	}
+
+	return
+}
+
+// Make sure to call this function with ch.BlockIndexAccess locked
 func (ch *Chain) PreCheckBlock(bl *btc.Block) (er error, dos bool, maybelater bool) {
 	// Size limits
 	if len(bl.Raw) < 81 {
